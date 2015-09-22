f3dfa44b61b9af33caa96e9533ffea3ed73a9506
Yiran Sheng <shengyiran@gmail.com>
2015-09-22 16:43:00 -0700

    finish mumbler

---
+++ mumbler.py
@@ -0,0 +1,39 @@
+import sys
+import random
+
+import search
+
+def mumbler(word, depth):
+    if depth < 1:
+        return word
+
+    next_words, next_words_count = search.search_next(word)
+    if next_words_count == 0:
+        return word
+
+    next_word = sample(next_words, next_words_count)
+
+    return word + " " + mumbler(next_word, depth-1)
+
+
+def sample(words, total_count):
+    threshold = random.randomint(0, total_count-1)
+    index = 0
+    for (word, count) in words.iteritems():
+        index += count
+        if threshold <= index:
+            return word
+
+if __name__ == '__main__':
+    if len(sys.argv) == 3:
+        start_word = sys.argv[1]
+        try:
+            start_count = int(sys.argv[2])
+        except ValueError:
+            sys.exit(1)
+    else:
+        sys.exit(1)
+
+    print mumbler(start_word, start_count - 1)
+
+
---
+++ search.py
@@ -0,0 +1,31 @@
+import os
+
+from collections import Counter
+
+import nodeconfig
+
+from pybloom import BloomFilter
+from parse import extract_next_words
+
+filters = []
+for i in xrange(0, 100):
+    filter_file = os.path.join(nodeconfig.GPFS_STORAGE,
+        "gram2_" + str(i) + ".processed.filter")
+    with open(filter_file, 'w') as f:
+        filters.append(BloomFilter.fromfile(f))
+
+def search_next(word):
+    counter = Counter()
+    next_words_count = 0
+    for i in xrange(0, 100):
+        if word in filters[i]:
+            data_file = os.path.join(nodeconfig.GPFS_STORAGE,
+                "gram2_" + str(i) + ".processed")
+            words, count = extract_next_words(datafile)
+            for w in words:
+                counter[w] += words[w]
+            next_words_count += count
+
+    return counter, next_words_count
+
+
