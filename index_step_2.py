d5278d1f92889f651ebf298c7120bfd8c3263344
Yiran Sheng <shengyiran@gmail.com>
2015-09-22 21:23:12 -0700

    fix index step2

--- index_step_2.py
+++ index_step_2.py
@@ -12,15 +12,19 @@ def build_master_index(where):
         print("Processing %s_index" % node)
         node_index_file = os.path.join(GPFS_STORAGE, "%s_index" % node)
         with open(node_index_file, 'r') as f:
-            index_entry = IndexEntry.unpack(f.read(size))
-            if index_entry.id not in master_index:
-                master_index[index_entry.id] = []
+            while True:
+                chunk = f.read(size)
+                if chunk == '':
+                    break
+                index_entry = IndexEntry.unpack(chunk)
+                if index_entry.id not in master_index:
+                    master_index[index_entry.id] = []
 
-            master_index[index_entry.id].append(
-                (index_entry.index,
-                 index_entry.offset,
-                 index_entry.chunk_size)
-            )
+                master_index[index_entry.id].append(
+                    (index_entry.index,
+                     index_entry.offset,
+                     index_entry.chunk_size)
+                )
 
     with open(where, 'w') as w:
         pickle.dump(master_index, w)
