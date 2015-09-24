d2b14f3c48f615e070a1d323eb54806461daa471
Yiran Sheng <shengyiran@gmail.com>
2015-09-24 12:44:42 -0700

    weird issues of executing script over ssh

--- index_step_2.py
+++ index_step_2.py
@@ -1,7 +1,10 @@
 import os
 
 import pickle
-from memcache import memcached
+try:
+    from memcache import memcached
+except ImportError::
+    pass
 
 from binindex import IndexEntry
 from nodeconfig import localnode, GPFS_STORAGE
@@ -28,11 +31,13 @@ def build_master_index(cache=False):
                 master_index[strid].append(index_content)
 
     print("finished loading index")
+    '''
     if cache:
         print("putting it to memcache")
         for key in master_index:
             memcached.set(key, master_index[key])
         print("done")
+    '''
     return master_index
 
 if __name__ == "__main__":
